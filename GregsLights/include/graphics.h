8c6b0c514aa63ff420218641c432b98d27d5e7d2
Greg Hormann <ghormann@gmail.com>
2015-08-16 12:40:40 -0400

    Format file

--- GregsLights/include/graphics.h
+++ GregsLights/include/graphics.h
@@ -3,7 +3,7 @@
 
 
 #ifndef WX_PRECOMP
-    #include <wx/wx.h>
+#include <wx/wx.h>
 #endif
 
 #include <wx/glcanvas.h>
@@ -19,31 +19,31 @@ class BasicGLPane : public wxGLCanvas
     wxGLContext*	m_context;
 
 public:
-	BasicGLPane(wxFrame* parent, int* args);
-	virtual ~BasicGLPane();
+    BasicGLPane(wxFrame* parent, int* args);
+    virtual ~BasicGLPane();
 
-	void resized(wxSizeEvent& evt);
+    void resized(wxSizeEvent& evt);
 
-	int getWidth();
-	int getHeight();
+    int getWidth();
+    int getHeight();
 
-	void render(wxPaintEvent& evt);
-	void prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y);
+    void render(wxPaintEvent& evt);
+    void prepare2DViewport(int topleft_x, int topleft_y, int bottomrigth_x, int bottomrigth_y);
 
-	// events
-	void mouseMoved(wxMouseEvent& event);
-	void mouseDown(wxMouseEvent& event);
-	void mouseWheelMoved(wxMouseEvent& event);
-	void mouseReleased(wxMouseEvent& event);
-	void rightClick(wxMouseEvent& event);
-	void mouseLeftWindow(wxMouseEvent& event);
-	void keyPressed(wxKeyEvent& event);
-	void keyReleased(wxKeyEvent& event);
+    // events
+    void mouseMoved(wxMouseEvent& event);
+    void mouseDown(wxMouseEvent& event);
+    void mouseWheelMoved(wxMouseEvent& event);
+    void mouseReleased(wxMouseEvent& event);
+    void rightClick(wxMouseEvent& event);
+    void mouseLeftWindow(wxMouseEvent& event);
+    void keyPressed(wxKeyEvent& event);
+    void keyReleased(wxKeyEvent& event);
 
-	DECLARE_EVENT_TABLE()
+    DECLARE_EVENT_TABLE()
 
-	private:
-        DisplayModel *model;
+private:
+    DisplayModel *model;
 };
 
 class RenderTimer : public wxTimer
