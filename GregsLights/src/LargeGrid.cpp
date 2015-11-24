f269b63e6416b7ce7a2fcdb7bdd0ea9634a855b5
Greg Hormann <ghormann@gmail.com>
2015-11-23 22:08:04 -0500

    House on Fire text

--- GregsLights/src/LargeGrid.cpp
+++ GregsLights/src/LargeGrid.cpp
@@ -329,6 +329,7 @@ void LargeGrid::test()
 
     while(1)
     {
+        scrollText(RGBColor::RED, RGBColor::DARKGREEN,"IS YOUR HOUSE ON FIRE CLARK? .... NO AUNT BETHANY, THOSE ARE THE CHRISTMAS LIGHTS.....", 0.02);
         peakSanta();
         RGBPicture *left = RGBPicture::getPicture(("train_blue_46.png"));
         RGBPicture *right = RGBPicture::getPicture(("train_right_46.png"));
