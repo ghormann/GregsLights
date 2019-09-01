92cf303c191451d5d15a88156f872b1283457f5d
Greg Hormann <ghormann@gmail.com>
2019-09-01 11:40:20 -0400

    Both snowmen and one Grid working

--- GregsLights/src/Snowmen.cpp
+++ GregsLights/src/Snowmen.cpp
@@ -73,16 +73,25 @@ Snowmen::Snowmen(bool skipTime,  E131Network *network[])
     printf("DEBUG: Snowmen Right Port: %d, channel: %d\n", port, channel);
 
     //Splash house aka Right
-    for (x = 0; x < SPLASH_GRID_WIDTH; x++)
+    y_delta = 1; // Will be switched at start -1 = down, 1 = up
+
+    for (x = SPLASH_GRID_WIDTH-1; x >= 0; x--)
     {
-        y_delta *= 1; // Swap direction, starts high
+        y_delta *= -1; // Swap direction
         y_start = (y_delta == 1 ? 0 : SPLASH_GRID_HEIGHT - 1);
         y_cnt = 0;
 
         for (y = y_start; y_cnt < SPLASH_GRID_HEIGHT; y += y_delta, ++y_cnt )
         {
-            splash[SNOWMAN_RIGHT]->setPixal(x,y, network[port]->getRGB(channel));
+            splash[SNOWMAN_RIGHT]->setPixal(x,SPLASH_GRID_HEIGHT-1 - y, network[port]->getRGB(channel));
             channel += 3;
+            pixels_in_string++;
+            if (pixels_in_string >= 1000)
+            {
+                pixels_in_string = 0;
+                channel = 0;
+                ++port;
+            }
             if (channel >= 510)
             {
                 channel = 0;
@@ -132,11 +141,58 @@ Snowmen::Snowmen(bool skipTime,  E131Network *network[])
         }
     }
 
-    for (x = 0; x < SNOWMEN_WIDTH; x++)
+    //Snowman left aka Road
+    y_delta = -1; // Will be switched at start -1 = down, 1 = up
+    port = 78; // Array is zero based. Port = 178
+    channel = 0; // also zero based.
+    pixels_in_string = 0;
+    for (x = 0; x <= SNOWMEN_WIDTH; x++)
     {
-        for (y = 0; y < SNOWMEN_HEIGHT; y++ )
+        y_delta *= -1; // Swap direction
+        y_start = (y_delta == 1 ? 0 : SNOWMEN_HEIGHT - 1);
+        y_cnt = 0;
+
+        for (y = y_start; y_cnt < SNOWMEN_HEIGHT; y += y_delta, ++y_cnt )
         {
-            snowmen[SNOWMAN_LEFT]->setPixal(x,y,new RGBLight(new DummyBulb(), new DummyBulb(), new DummyBulb()));
+            snowmen[SNOWMAN_LEFT]->setPixal(x,SNOWMEN_HEIGHT-1 - y, network[port]->getRGB(channel));
+            printf("X: %d, Y: %d, port: %d, channel: %d\n", x,y,port,channel);
+            channel += 3;
+            pixels_in_string++;
+            if (pixels_in_string >= 1000)
+            {
+                pixels_in_string = 0;
+                channel = 0;
+                if (port > 78) // really 178
+                {
+                    port = 72; // really 172
+                }
+                else if (port > 72) // really 172
+                {
+                    port = 66; // really 166
+                }
+                else if (port > 66)
+                {
+                    port = 60;
+                }
+                else if (port > 60)
+                {
+                    port = 54;
+                }
+                else if (port < 54)
+                {
+                    printf("Error settng up Left snowman.\n");
+                    throw "Error setting up Left Snowmen.";
+                }
+                else
+                {
+                    ++port;
+                }
+            }
+            if (channel >= 510)
+            {
+                channel = 0;
+                ++port;
+            }
         }
     }
 
@@ -233,21 +289,30 @@ void Snowmen::releaseSnowmen()
 
 void Snowmen::test_snowmen()
 {
-    while(1)
+    while(0)
     {
         this->run();
     }
     while(1)
     {
-        SnowmenGrid *grid = getSnowmen(SNOWMAN_RIGHT);
-        grid->setBackground(RGBColor::BLUE);
+        getSnowmen(SNOWMAN_RIGHT)->setBackground(RGBColor::BLUE);
+        getSnowmen(SNOWMAN_LEFT)->setBackground(RGBColor::BLUE);
+        getSplashGrid(SNOWMAN_RIGHT)->setBackground(RGBColor::BLUE);
+        getSplashGrid(SNOWMAN_LEFT)->setBackground(RGBColor::BLUE);
+
         write_data(2.0);
         for (int x = SNOWMEN_WIDTH-1; x >= 0; x--)
         {
             RGBColor *color = ((x%2) == 0 ? RGBColor::GREEN : RGBColor::RED);
             for (int y = 0; y < SNOWMEN_HEIGHT; y++)
             {
-                grid->getPixal(x,y)->set(color);
+                getSnowmen(SNOWMAN_LEFT)->getPixal(x,y)->set(color);
+                getSnowmen(SNOWMAN_RIGHT)->getPixal(x,y)->set(color);
+                if (x < SPLASH_GRID_WIDTH && y < SPLASH_GRID_HEIGHT)
+                {
+                    getSplashGrid(SNOWMAN_LEFT)->getPixal(x,y)->set(color);
+                    getSplashGrid(SNOWMAN_RIGHT)->getPixal(x,y)->set(color);
+                }
                 write_data(0.005);
             }
         }
